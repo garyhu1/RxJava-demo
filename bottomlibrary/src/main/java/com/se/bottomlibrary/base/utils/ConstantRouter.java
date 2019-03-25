e8d17801eba8e448ccd6df7b12333cc8c0845405
huweiwei <huweiwei@superengine.com.cn>
2019-03-25 18:20:15 +0800

    修改module名称

---
+++ bottomlibrary/src/main/java/com/se/bottomlibrary/base/utils/ConstantRouter.java
@@ -0,0 +1,10 @@
+package com.se.bottomlibrary.base.utils;
+
+/**
+ * Created by yb on 2019/3/25.
+ */
+
+public class ConstantRouter {
+
+    public static final String BASE_LOGIN = "/base/login";
+}
